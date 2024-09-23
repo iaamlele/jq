5f152c9c6bbcc302f0479a50a33c67ad6769fa25
Le Chen <LeChen11111@163.com>
2024-09-23 11:28:01 +0800

    demo: Modify the demo test case

--- demo/index.js
+++ demo/index.js
@@ -2,7 +2,12 @@ import $ from '../src/index.js'
 
 console.log('js开始了');
 
-$();
+$('.class-test').hide();
+const node_list = document.getElementsByClassName('class-test');
+console.log(node_list);
+for(let i = 0; i < node_list.length; i++) {
+    console.log(node_list[i].style.visibility);
+}
 
 // $(document).ready(function() {
 //    $('.class-test').hide().show();
